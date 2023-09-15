9fbda08c0af23ae6de43012d5dd56859c2769123
kanta <kanta@localhost>
2023-09-15 22:35:21 +0900

    問1

---
+++ HelloWorld_Tamai/src/curriculum_A/Qes1_13.java
@@ -0,0 +1,18 @@
+package curriculum_A;
+
+public class Qes1_13 {
+
+	public static void main(String[] args) {
+		// コメントをここに記載
+		byte b;
+		short s;
+		int i;
+		long l;
+		float f;
+		double d;
+		char c;
+		String str;
+		boolean bl;
+	}
+
+}
